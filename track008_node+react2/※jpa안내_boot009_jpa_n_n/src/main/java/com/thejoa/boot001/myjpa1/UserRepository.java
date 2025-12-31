f080546fe514c6fbddbcb8402e5ad886a6485e78
HyoJung An <sally03915@gmail.com>
2025-12-31 21:04:50 +0900

    Delete track008_node+react2 directory

--- track008_node+react2/※jpa안내_boot009_jpa_n_n/src/main/java/com/thejoa/boot001/myjpa1/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package com.thejoa.boot001.myjpa1;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository  extends JpaRepository<User, Long> {}
--- track008_node+react2/※jpa안내_boot009_jpa_n_n/src/main/java/com/thejoa/boot001/myjpa2/StudentRepository.java
+++
@@ -1,5 +0,0 @@
-package com.thejoa.boot001.myjpa2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository  extends JpaRepository<Student, Long> {}
